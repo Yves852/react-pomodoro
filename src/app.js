1569f991895624bf688c9fd74a0e7c09b9b2708e
yves852 <thirion.yves@gmail.com>
2021-10-18 16:41:57 +0200

    Added timer display with basic position style

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import PomodoroTimer from "./components/pomodoro-timer";
 export default function App() {
-    return <div className={"App"}>{"Hello"}</div>;
+    return (
+        <div className={"App"}>
+            <h1>{"Pomodoro"}</h1>
+            <PomodoroTimer />
+        </div>
+    );
 }
